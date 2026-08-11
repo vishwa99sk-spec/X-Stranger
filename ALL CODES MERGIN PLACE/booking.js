5e9155f7b3359ff2dab67c53852b04ca05a50cbc
vishwa99sk-spec <vishwa99sk@gmail.com>
2026-08-11 20:13:30 +0530

    vishwa day 6 work evening

---
+++ ALL CODES MERGIN PLACE/booking.js
@@ -0,0 +1,6 @@
+
+    var btn1 = document.getElementById("sendrequestbtn")
+
+    btn1.addEventListener("click",function(){
+        alert("Your request was sucessfully submitted")
+    })
---
+++ ALL CODES MERGIN PLACE/skillpoints.js
@@ -0,0 +1,6 @@
+
+    var btn2 = document.getElementById("sub-btn")
+
+    btn2.addEventListener("click",function(){
+        alert("Your Rating was sucessfully submitted")
+    })
